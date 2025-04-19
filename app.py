82aecbb039082822a56b478c9afdede1ae4e3a5d
smrvaghefi <smr_vaghefi2000@yahoo.com>
2025-04-19 15:15:39 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from tabpy.tabpy_server.app.app import TabPyApp
-from tabpy.tabpy_server.app.util import parse_pwd_file
 import os
 
-# تنظیمات امنیتی (اختیاری)
-os.environ["TABPY_PWD_FILE"] = "passwords.txt"  # فایل رمز عبور
+# مسیر فایل کانفیگ
+config_file = os.path.join(os.path.dirname(__file__), 'tabpy.conf')
 
-# اجرای سرور TabPy
-app = TabPyApp()
-app.run()
+# تنظیم پورت برای Render
+port = int(os.environ.get("PORT", 9004))
+
+# اجرای سرور
+app = TabPyApp(config_file=config_file)
+app.run(host='0.0.0.0', port=port)
