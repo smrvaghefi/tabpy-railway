6394fd4c8498260f11bf62ac72b6931164bf67eb
smrvaghefi <smr_vaghefi2000@yahoo.com>
2025-04-19 17:46:26 +0330

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ import os
 config_file = os.path.join(os.path.dirname(__file__), 'tabpy.conf')
 port = int(os.environ.get("PORT", 9004))
 
-
+os.environ['TABPY_PWD_FILE'] = 'admin:admin123'
 app = TabPyApp(config_file=config_file)
 app.run()
