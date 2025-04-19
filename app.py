1f8a6326b2b7694536ad3790904bea95ac3df2b7
smrvaghefi <smr_vaghefi2000@yahoo.com>
2025-04-19 18:21:16 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from tabpy.tabpy_server.app.app import TabPyApp
 import os
 
-# ایجاد فایل رمز عبور با فرمت صحیح
-with open('/tmp/tabpy_pass.txt', 'w') as f:
-    f.write("admin:admin123")  # بدون هیچ کاراکتر اضافه
+with open('pass.txt', 'w') as f:
+    f.write("admin:admin123")
     
 # اجرای سرور
 app = TabPyApp(config_file='tabpy.conf')
