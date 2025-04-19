8944202fab1e8430064c4c7e01afa2475193e692
smrvaghefi <smr_vaghefi2000@yahoo.com>
2025-04-19 18:12:25 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from tabpy.tabpy_server.app.app import TabPyApp
 import os
-from pathlib import Path
 
 # ایجاد فایل رمز عبور با فرمت صحیح
-password_file = Path('/tmp/tabpy_passwords.txt')
-password_file.write_text("admin:admin123")  # فقط این خط بدون هیچ کاراکتر اضافه
-
-# تأیید محتوای فایل (برای دیباگ)
-print("Password file content:", repr(password_file.read_text()))
-
+with open('/tmp/tabpy_pass.txt', 'w') as f:
+    f.write("admin:admin123")  # بدون هیچ کاراکتر اضافه
+    
 # اجرای سرور
 app = TabPyApp(config_file='tabpy.conf')
 app.run()
